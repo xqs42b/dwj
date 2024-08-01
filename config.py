5075b3e956122b91e30d48f98a8a7f3b6c108266
xqs <501919337@qq.com>
2024-08-01 21:59:41 +0800

    增加配置功能

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    # 数据库配置
+    DB_SETTINGS = {
+        "DB_HOST": "192.168.1.123",
+        "DB_NAME": "wj",
+        "DB_USER": "xqs"
+    }
+    A = 200
--- server.py
+++ server.py
@@ -1,4 +1,19 @@
+import os
 from sanic import Sanic
+from sanic.response import text
+from config import Config
+
+
+exec_path = os.path.dirname(__file__)
+config_name = "config.py"
+config_path = f"{exec_path}/{config_name}"
+print(config_path)
 
 app = Sanic("myApp")
+app.update_config(Config)
+print(app.config.A)
+print(app.config.DB_SETTINGS)
 
+@app.get("/")
+async def hi(request):
+    return text("hi china")
