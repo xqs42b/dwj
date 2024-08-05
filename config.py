e0dabce9dde59b13b7e06137f436455ac20c2dca
xqs <501919337@qq.com>
2024-08-05 22:00:16 +0800

    增加配置选项

--- config.py
+++ config.py
@@ -5,4 +5,16 @@ class Config:
         "DB_NAME": "wj",
         "DB_USER": "xqs"
     }
-    A = 200
+    DB_CONN_STR = "postgresql+psycopg2://username:password@localhost/dbname"
+
+    # REDIS数据库连接配置
+    REDIS_SETTINGS = {
+
+    }
+    REDIS_CONN_STR = "redis://{CONFIG.REDIS_HOST}:{CONFIG.REDIS_PORT}/{CONFIG.REDIS_DB}"
+
+    # DEBUG配置
+    DEBUG = True
+
+    # 修改代码后自动加载
+    AUTO_RELOAD = True
